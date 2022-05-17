8d8abd4a8294f3294b08c66d7ad19aff43c49563
PedroSantana <phsantana99@gmail.com>
2022-05-17 14:12:31 -0300

    exercicios dia 4.1 feitos 100%, sem duvidas

---
+++ fundamentos-dev-web/b4-introducao-js-logica/operadoresLogicos2.js
@@ -0,0 +1,14 @@
+let weekDay = 'sabado';
+
+if (weekDay === 'segunda-feira' || weekDay === 'terça-feira' || weekDay === 'quarta-feira' || weekDay === 'quinta-feira' || weekDay === 'sexta-feira') {
+    console.log ('Mais um dia de aprendizado na Trybe!') }
+else 
+console.log ('Fim de semana, descanse!')
+
+
+console.log((2 + 2) === 4);
+
+
+console.log(!(2 + 2) === 4);
+
+
