f2a74ea642a00a5bc3799b1bb8b20e97d8ea2bdb
PedroSantana <phsantana99@gmail.com>
2022-05-20 13:38:44 -0300

    falta finalizar exercicio 3 e 4

--- fundamentos-dev-web/b4-introducao-js-logica/dia-3/exercicios.js
+++ fundamentos-dev-web/b4-introducao-js-logica/dia-3/exercicios.js
@@ -8,3 +8,24 @@ console.log(result);
 
 //exercicio 2
 let word = 'tryber';
+let inverse = '';
+for (let i = word.length -1; i >= 0; i -=1 ){
+        inverse += word[i];
+}
+console.log(inverse);
+
+//exercicio 3
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+let biggest = array[0];
+function biggestWord (array) {
+    for (let index = 0; index = array.length -1; index =+1) {
+        if (array[index] > biggest.length) {
+            biggest = array[index];
+        }
+        return biggest;
+    }
+}
+console.log(biggest);
+
+//exercicio 4
+
