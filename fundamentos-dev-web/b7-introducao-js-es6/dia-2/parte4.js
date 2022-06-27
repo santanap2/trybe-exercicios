fa625603b99db7ea2956720257bfebf18e67b533
PedroSantana <phsantana99@gmail.com>
2022-06-27 20:12:26 -0300

    atualizacao repositorio exercicios

---
+++ fundamentos-dev-web/b7-introducao-js-es6/dia-2/parte4.js
@@ -0,0 +1,23 @@
+
+// const person = {
+//   name:'Roberto',
+// };
+
+// const lastName = {
+//   lastName: 'Silva',
+// };
+
+// const newPerson = Object.assign({},person,lastName);
+// newPerson.name = 'Gilberto';
+// console.log(newPerson);
+// console.log(person);
+
+const getDayName = (number) => {
+  const dayNumber = number - 1;
+  const days = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'];
+  return days[dayNumber];
+};
+
+const printDayname = (dayNumber) => {
+  console.log(getDayName(dayNumber));
+};
---
+++ fundamentos-dev-web/b8-higher-order-functions-js-es6/dia-2/someAndEvery.js
@@ -0,0 +1,40 @@
+// const listNames = ['Maria', 'Manuela', 'Jorge', 'Ricardo', 'Wilson'];
+
+// const verifyFirstLetter = (letter, names) => names.some((name) => name[0] === letter);
+
+// console.log(verifyFirstLetter('J', listNames)); // true
+// console.log(verifyFirstLetter('x', listNames)); // false
+
+// const grades = {
+//   portugues: 'Aprovado',
+//   matematica: 'Reprovado',
+//   ingles: 'Aprovado',
+// };
+
+// const verifyGrades = (studentGrades) => (
+//   Object.values(studentGrades).every((grade) => grade === 'Aprovado')
+// );
+
+// console.log(verifyGrades(grades));
+
+const names = ['Mateus', 'José', 'Ana', 'Cláudia', 'Bruna'];
+
+const hasName = (arr, name) => {
+  return names.some((elem) => elem === name);
+}
+
+console.log(hasName(names, 'Ana'))
+
+const people = [
+  { name: 'Mateus', age: 18 },
+  { name: 'José', age: 16 },
+  { name: 'Ana', age: 23 },
+  { name: 'Cláudia', age: 20 },
+  { name: 'Bruna', age: 19 },
+];
+
+const verifyAges = (arr, minimumAge) => {
+  return people.every((person) => person.age >= minimumAge)
+}
+
+console.log(verifyAges(people, 18));
